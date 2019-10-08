ef368cd6e97b56ba0dbefb3bc3189dd609117608
Дмитрий <daniel24111993@mail.ru>
2019-10-08 11:52:40 +0300

    first attempt

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.minNumber = 0;
+    	this.maxNumber = 0;
+    	this.targetNumber = 0;
+    }
 
     setRange(min, max) {
+      this.minNumber = min;
+      this.maxNumber = max;
+      
 
     }
 
     guess() {
-
+      this.targetNumber = Math.floor((this.maxNumber + this.minNumber) / 2);
+      return this.targetNumber;
     }
 
     lower() {
-
+      this.maxNumber = this.targetNumber;
     }
 
     greater() {
-
+      this.minNumber = this.targetNumber;
     }
 }
 
